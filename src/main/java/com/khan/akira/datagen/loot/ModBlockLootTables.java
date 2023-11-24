92da70bcc0ff8b26d57ad8b9054dc6935aac741b
UzKhan2 <uzairakhan82@gmail.com>
2023-11-24 17:19:31 -0500

    Update

--- src/main/java/com/khan/akira/datagen/loot/ModBlockLootTables.java
+++ src/main/java/com/khan/akira/datagen/loot/ModBlockLootTables.java
@@ -17,37 +17,68 @@ import net.minecraftforge.registries.RegistryObject;
 import java.util.Set;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
-    public ModBlockLootTables() {
-        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
-    }
+        public ModBlockLootTables() {
+                super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+        }
 
-    @Override
-    protected void generate() {
-        this.dropSelf(ModBlocks.SAPPHIRE_BLOCK.get());
-        this.dropSelf(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
-        this.dropSelf(ModBlocks.SOUND_BLOCK.get());
+        @Override
+        protected void generate() {
+                this.dropSelf(ModBlocks.SAPPHIRE_BLOCK.get());
+                this.dropSelf(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+                this.dropSelf(ModBlocks.SOUND_BLOCK.get());
 
-        this.add(ModBlocks.SAPPHIRE_ORE.get(),
-                block -> createCopperLikeOreDrops(ModBlocks.SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
-        this.add(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
-                block -> createCopperLikeOreDrops(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
-        this.add(ModBlocks.NETHER_SAPPHIRE_ORE.get(),
-                block -> createCopperLikeOreDrops(ModBlocks.NETHER_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
-        this.add(ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
-                block -> createCopperLikeOreDrops(ModBlocks.END_STONE_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
+                this.add(ModBlocks.SAPPHIRE_ORE.get(),
+                                block -> createCopperLikeOreDrops(ModBlocks.SAPPHIRE_ORE.get(),
+                                                ModItems.RAW_SAPPHIRE.get()));
+                this.add(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                                block -> createCopperLikeOreDrops(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                                                ModItems.RAW_SAPPHIRE.get()));
+                this.add(ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                                block -> createCopperLikeOreDrops(ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                                                ModItems.RAW_SAPPHIRE.get()));
+                this.add(ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                                block -> createCopperLikeOreDrops(ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                                                ModItems.RAW_SAPPHIRE.get()));
 
-    }
+                this.dropSelf(ModBlocks.SAPPHIRE_STAIRS.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_BUTTON.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_PRESSURE_PLATE.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_TRAPDOOR.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_FENCE.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_WALL.get());
 
-    protected LootTable.Builder createCopperLikeOreDrops(Block pBlock, Item item) {
-        return createSilkTouchDispatchTable(pBlock,
-                this.applyExplosionDecay(pBlock,
-                        LootItem.lootTableItem(item)
-                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 5.0F)))
-                                .apply(ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE))));
-    }
+                this.add(ModBlocks.SAPPHIRE_SLAB.get(),
+                                block -> createSlabItemTable(ModBlocks.SAPPHIRE_SLAB.get()));
+                this.add(ModBlocks.SAPPHIRE_DOOR.get(),
+                                block -> createDoorTable(ModBlocks.SAPPHIRE_DOOR.get()));
+                this.dropSelf(ModBlocks.SAPPHIRE_STAIRS.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_BUTTON.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_PRESSURE_PLATE.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_TRAPDOOR.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_FENCE.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+                this.dropSelf(ModBlocks.SAPPHIRE_WALL.get());
 
-    @Override
-    protected Iterable<Block> getKnownBlocks() {
-        return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
-    }
+                this.add(ModBlocks.SAPPHIRE_SLAB.get(),
+                                block -> createSlabItemTable(ModBlocks.SAPPHIRE_SLAB.get()));
+                this.add(ModBlocks.SAPPHIRE_DOOR.get(),
+                                block -> createDoorTable(ModBlocks.SAPPHIRE_DOOR.get()));
+
+        }
+
+        protected LootTable.Builder createCopperLikeOreDrops(Block pBlock, Item item) {
+                return createSilkTouchDispatchTable(pBlock,
+                                this.applyExplosionDecay(pBlock,
+                                                LootItem.lootTableItem(item)
+                                                                .apply(SetItemCountFunction.setCount(
+                                                                                UniformGenerator.between(2.0F, 5.0F)))
+                                                                .apply(ApplyBonusCount.addOreBonusCount(
+                                                                                Enchantments.BLOCK_FORTUNE))));
+        }
+
+        @Override
+        protected Iterable<Block> getKnownBlocks() {
+                return ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+        }
 }
--- src/main/java/com/khan/akira/item/ModCreativeModeTabs.java
+++ src/main/java/com/khan/akira/item/ModCreativeModeTabs.java
@@ -31,6 +31,18 @@ public class ModCreativeModeTabs {
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
                         pOutput.accept(ModItems.STRAWBERRY.get());
                         pOutput.accept(ModItems.PINE_CONE.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_STAIRS.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_SLAB.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_BUTTON.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_PRESSURE_PLATE.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_FENCE.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_WALL.get());
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_DOOR.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_TRAPDOOR.get());
                     })
                     .build());
 
